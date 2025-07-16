dda13c239a168aa6a4194426c3a31b7be20fc433
João Vitor Carvalho Barbosa <joaovitorcarvalho903@gmail.com>
2025-07-16 16:59:20 -0300

    feat: configurar exibição no admin

--- escola/admin.py
+++ escola/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Estudante, Curso
 
-# Register your models here.
+class EstudanteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'email', 'cpf', 'data_nascimento', 'celular')
+    list_display_links = ('id', 'nome')
+    list_per_page = 20
+    search_fields = ('nome',)
+    
+admin.site.register(Estudante, EstudanteAdmin)
+
+class CursoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'codigo', 'descricao')
+    list_display_links = ('id', 'codigo')
+    search_fields = ('codigo',)
+    
+admin.site.register(Curso, CursoAdmin)
